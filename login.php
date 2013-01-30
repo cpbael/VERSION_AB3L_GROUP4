96d84fb699d206a103f1ed32168e03004f5addca
kevinromas <kevinromas@gmail.com>
2013-01-30 02:47:37 -0800

    Create Log In Page

---
+++ login.php
@@ -0,0 +1,50 @@
+<html>
+  <head>
+		<link rel="stylesheet" type="text/css" href="Style.css"/>
+	</head>
+	
+	<body>
+<div class="log3">
+<table class = "loginBottom"> 
+	<form name = "add" method = "POST" action = "">	
+		<tr>
+			<td colspan = "2"><hr><td>
+		</tr>
+	
+	</br>
+
+	
+	<tr>
+	<td class = "loginBottom1">Username: </td>
+	<td><input class = "form"type="text" name="middleinitial" value="" size="35" />
+	</td>
+	
+	<tr>
+		<td class = "loginBottom1">Password: </td>
+		<td><input class = "form"type="password" name="pwdconfirm" value="" size="35" />
+	</td>
+	
+	<tr>
+
+		<tr>
+			<td colspan = "2" class = "loginBottom3"><span class = "textBluer">Forget Password?</span></td>
+		</tr>
+		
+	<tr>
+	
+	<td colspan = "2" class = "loginBottom1">
+		<input class="login" type="submit" value="Log In" name = "login" />
+	</td>
+	</tr>
+	
+		<tr>
+			<td colspan = "2"><hr><td>
+		</tr>
+</table> 	
+	
+	
+	</form>
+	</div>
+	</body>
+	
+</html>
