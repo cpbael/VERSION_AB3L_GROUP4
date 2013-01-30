a0f9793e2162831979c33986ecf45b9a7ed5f41c
kevinromas <kevinromas@gmail.com>
2013-01-30 02:23:56 -0800

    Create Sign Up page
    with css file

---
+++ sign_up.php
@@ -0,0 +1,107 @@
+<html>
+  <head>
+		<link rel="stylesheet" type="text/css" href="Style.css"/>
+	</head>
+	
+	<body>
+<div class="log3">
+<table class = "loginBottom"> 
+	<form name = "add" method = "POST" action = "">
+		
+		<tr>
+			<div id = "signup">
+				Sign Up Form
+			</div>
+		</tr>
+		
+		<tr>
+			<td colspan = "2"><hr><td>
+		</tr>
+	
+	</br>
+	
+	<tr>
+	<td class = "loginBottom1">Email Address: </td> 
+		<td><input class = "form" type="text" name="idnum" value="" size="35" />
+		</td>
+	</tr>
+
+	
+	</br>
+	
+	<tr>
+	<td class = "loginBottom1">Name: </td> 
+		<td><input class = "form" type="text" name="firstname" value="" size="35" />
+		</td>
+	</tr>
+	<!--kung paghihiwalayin natin ung name	
+	<tr>
+		<td class = "loginBottom1">Middle Name: </td> 
+		<td><input class = "form" type="text" name="firstname" value="" size="35" />
+		</td>
+	</tr>
+	
+	<tr>
+		<td class = "loginBottom1">Last Name: </td> 
+		<td><input class = "form" type="text" name="firstname" value="" size="35" />
+		</td>
+	</tr>
+	-->
+	</br>
+	
+	
+	<tr>
+	<td class = "loginBottom1">Username: </td>
+	<td><input class = "form"type="text" name="username" value="" size="35" />
+	</td>
+	
+	<tr>
+		<td class = "loginBottom1">Password: </td>
+		<td><input class = "form"type="password" name="pwd" value="" size="35" />
+	</td>
+	
+	<tr>
+		<td class = "loginBottom1">Confirm Password: </td>
+		<td><input class = "form"type="password" name="pwdconfirm" value="" size="35" />
+	</td>
+
+	</br>
+	
+	
+	<tr>
+	<td class = "loginBottom1">Contact Number:</td>
+	<td><input class = "form"type="text" name="contact" value="" size="35" />
+	</td>
+
+	</br>
+		
+	<tr>
+	<td class = "loginBottom1">Gender:</td>
+	<td><select class="form" name= "gender">
+	  <option value="NOVALUE" ></option>
+	  <option value="MALE" >MALE</option>
+	  <option value="FEMALE" >FEMALE</option>
+	</select> </td></tr>
+	
+	</br>
+	
+	<tr>
+
+		<tr>
+			<td colspan = "2" class = "loginBottom3"><span class = "textBlack">I </span><span class = "textBluer">CERTIFY</span> <span class = "textBlack">that all the information given in this form <br/>are true and correct.</span></td>
+		</tr>
+		
+	<tr>
+	
+	<td colspan = "2" class = "loginBottom1">
+		<input id="mysubmit" type="submit" value="Submit" name = "sub" />
+	</td>
+	</tr>
+</table> 	
+	
+	
+	</form>
+	</div>
+	</body>
+	
+</html>
