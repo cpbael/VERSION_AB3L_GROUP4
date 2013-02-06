fb135bd3e1be19dc05c973c4e44b179fa5ce71ac
Ybur Haniel <astarialexi@gmail.com>
2013-02-05 18:33:24 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,37 @@
 <?php
 	session_start();
+	$link = mysql_connect('localhost', 'root', '');
+		if (!$link) {
+			die('Could not connect to mysql: ' . mysql_error());
+		}else{
+			$db = mysql_select_db("hrm", $link); 
+		
+			if (!$db) {
+				die('Could not connect to database: ' . mysql_error());
+			}
+		}
 	if(isset($_POST['login'])){
-		//Pwede bag ganito yung pagconfirm ng account ng admin? Hhe.
-		if($_POST['middleinitial']=='admin' && $_POST['pwdconfirm']=='hotel128'){
-			$_SESSION['login']=1;
-			header('Location: admin.php');
-			exit;
-		}else echo 'Invalid username/password';
+		//Member
+		$result = mysql_query("SELECT * FROM member");
+		while($row = mysql_fetch_array($result)){
+			if($_POST['middleinitial']==$row['uname'] && $_POST['pwdconfirm']==$row['password']){
+				$_SESSION['login']=1;
+				header('Location: home.php');
+				exit;
+			}
+		}
+		
+		//Admin
+		$result = mysql_query("SELECT * FROM admin");
+		$row = mysql_fetch_array($result);
+		if($_POST['middleinitial']==$row['username'] && $_POST['pwdconfirm']==$row['password']){
+				$_SESSION['login']=1;
+				header('Location: admin.php');
+				exit;
+		}
 	}
+	
+	mysql_close($link);
 ?>
 
 <html>
